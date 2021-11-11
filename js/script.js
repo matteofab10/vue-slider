6fed2a136af8718d88034bc6b760c5c18bba6a33
matteofab10 <matteofabio10@gmail.com>
2021-11-11 16:09:55 +0100

    creazione progetto, modifiche js/vue

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const app = new Vue ({
+
+  el: '#app',
+
+  data: {
+    images: ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
+    title: ['Svezia','Svizzera','Gran Bretagna','Germania','Paradise'],
+    text: ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.','Lorem ipsum','Lorem ipsum, dolor sit amet consectetur adipisicing elit.','Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,','Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'],
+    counter: 0
+  },
+  methods: {
+    upSlide(){
+      this.counter++;
+      if(this.counter > this.images.length - 1){
+        this.counter = 0;
+      }
+    },
+
+    downSlide(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.images.length - 1;
+      }
+    }
+  }
+})
